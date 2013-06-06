7dc2dc281c56b49f226f94a5c9de6a7be233d6c4
StephanieFrias <stephanie.frias@gmail.com>
2013-06-06 17:07:21 -0400

    Week 0

--- swampy-package/swampy/sib_flower.py
+++ swampy-package/swampy/sib_flower.py
@@ -1,6 +1,6 @@
 # Flower excercise (4.2) from Week 0
 
-# Name:
+# Name: Stephanie Frias
 
 
 from TurtleWorld import * 		
@@ -11,12 +11,50 @@ bob = Turtle()
 
 # This is where you put code to move bob
 
+bob.delay = 0.01
 
+def polyline(t,n,length,angle):
+	"""With a turtle object, t, draw n lines of length l, and turn angle a degrees before drawing next line"""
+	for i in range(n):
+		fd(t,length)
+		lt(t,angle)
 
+import math
+print math
 
+def arc(t,r,angle):
+	"""Draw an arc with radius r with subtended angle"""
+	arc_length= math.pi * 2 * r* abs(angle) / 360
+	n= int(arc_length/4)+1
+	angle=360.0/n
+	step_length=arc_length/n
+	step_angle=float(angle)/n
+	polyline(t,n,step_length,step_angle)
 
+def petal(t, r, angle):
+	"""Draw a petal with 2 arcs"""
+	for i in range(2):
+		arc(t, r, angle)
+		lt(t, 180-angle)
 
+def flower(t, n, r, angle):
+	for i in range(n):
+		petal(t, r, angle)
+		lt(t, 360.0/n)
 
+def move(t, length):
+	pu(t)
+	fd(t,length)
+	pd(t)
+
+move(bob,100)
+flower(bob,7,60.0,60.0)
+
+move(bob,100)
+flower(bob,10,40.0,80.0)
+
+move(bob,100)
+flower(bob,20,140.0,20.0)
 
 wait_for_user()					
 
--- swampy-package/swampy/sib_polygon.py
+++ swampy-package/swampy/sib_polygon.py
@@ -1,6 +1,6 @@
 # Polygon excercise from Week 0
 
-# Name:
+# Name: Stephanie Frias
 
 
 from TurtleWorld import * 		
@@ -11,11 +11,48 @@ bob = Turtle()
 
 # This is where you put code to move bob
 
+bob.delay = 0.01
 
+def square(t,l):
+	for i in range(4):
+		fd(t, l)
+		lt(t)
+square (bob,50)
+#to test the code and close the function and loop, the above example produces a square with sides of length 50
 
+def polyline(t,n,length,angle):
+	"""With a turtle object, t, draw n lines of length l, and turn angle a degrees before drawing next line"""
+	for i in range(n):
+		fd(t,length)
+		lt(t,angle)
 
+def polygon (t,length,n):
+	"""Draw a polygon with n sides, each of length l"""
+	angle=360.0/n
+	polyline(t,n,length,angle)
+polygon(bob,30,3)
 
+import math
+print math
 
+def circle(t,r):
+	"""Draw a circle, with radius r"""
+	circumference = math.pi * 2 * r
+	n= int(circumference/3)+1
+	angle=360.0/n
+	length= int(circumference/n)
+	polyline(t,n,length,angle)
+circle(bob,30)
+
+def arc(t,r,angle):
+	"""Draw an arc with radius r with subtended angle"""
+	arc_length= math.pi * 2 * r* abs(angle) / 360
+	n= int(arc_length/4)+1
+	angle=360.0/n
+	step_length=arc_length/n
+	step_angle=float(angle)/n
+	polyline(t,n,step_length,step_angle)
+arc(bob,60,40)
 
 
 wait_for_user()					
